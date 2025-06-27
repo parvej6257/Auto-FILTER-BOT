ca12f712d63607e370e9fa89281ca78b7a82b1bc
parvej6257 <aditikaur1017@gmail.com>
2025-06-27 23:45:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -31,7 +31,7 @@ FSUB_IMG = (environ.get('FSUB_IMG', 'https://i.ibb.co/cShkPjcZ/x.jpg')).split()
 
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5654685686').split()] 
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-100').split()]
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002413284040'))  
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002737919350'))  
 BIN_CHANNEL = int(environ.get('BIN_CHANNEL', '-100'))  
 MOVIE_UPDATE_CHANNEL = int(environ.get('MOVIE_UPDATE_CHANNEL', '-100'))  
 PREMIUM_LOGS = int(environ.get('PREMIUM_LOGS', '-100')) 
